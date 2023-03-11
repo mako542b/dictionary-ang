19480cf61f256f60403756c463e172887abe1ec2
mako542b <marcin.piotr.konopka@gmail.com>
2023-03-11 22:27:40 +0100

    loading state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,23 +16,32 @@ export class AppComponent {
   chosenFont: string = 'Sans Serif'
   // darkTheme: boolean =  window.matchMedia(“(prefers-color-scheme: dark)”);
   darkMode = false
+  isLoading = false
 
-
+  latestCall: any = null
 
   onInput(event: Event) {
     this.error = false
     const param = (event.target as HTMLInputElement).value
-    this.http.get('https://api.dictionaryapi.dev/api/v2/entries/en/' + param)
-      .subscribe({ next: this.handleInput.bind(this), error: this.handleError.bind(this)})
+    if(this.latestCall) clearTimeout(this.latestCall)
+    this.isLoading = true
+    this.latestCall = setTimeout(() => {
+      this.http.get('https://api.dictionaryapi.dev/api/v2/entries/en/' + param)
+        .subscribe({ next: this.handleInput.bind(this), error: this.handleError.bind(this)})
+    },500)
+
   }
 
   handleInput(response: any) {
     this.responseList = response[0]
+    this.isLoading = false
+
   }
 
   handleError({ error }: any) {
       this.error = true
-      this.responseList = null    
+      this.responseList = null
+      this.isLoading = false
   }
 
 }
